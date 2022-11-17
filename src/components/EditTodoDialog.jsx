6d3e621ac62a2ee6723a78ff4195c3c8d87ee884
DohertyRen <dohertyrenato@gmail.com>
2022-11-16 23:19:38 -0300

    first commit

---
+++ src/components/EditTodoDialog.jsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import { TextField } from "@mui/material";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+
+export default function EditTodoDialog({
+  open,
+  dialogHandler,
+  todo,
+  editTodo,
+}) {
+  const [editedText, setEditedText] = useState(todo.text);
+
+  const textHandler = () => {
+    editTodo(todo.id, editedText);
+    dialogHandler();
+  };
+
+  return (
+    <Dialog
+      open={open}
+      onClose={dialogHandler}
+      aria-labelledby="alert-dialog-title"
+      aria-describedby="alert-dialog-description"
+      fullWidth
+    >
+      <DialogTitle id="alert-dialog-title">{"Editando tarefa"}</DialogTitle>
+      <DialogContent>
+        <TextField
+          fullWidth
+          defaultValue={editedText}
+          onChange={(e) => setEditedText(e.target.value)}
+        />
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={dialogHandler}>Cancelar</Button>
+        <Button autoFocus onClick={textHandler}>
+          Editar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+}
